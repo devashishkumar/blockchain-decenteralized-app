eed45336c3000f8fb3f5a87586323aeb7bd6461e
devashishkumar <dev.ashishkumar0512@gmail.com>
2022-02-12 16:19:19 +0530

    connect angular application to metamask

--- dapp-frontend/src/app/app.component.ts
+++ dapp-frontend/src/app/app.component.ts
@@ -10,31 +10,67 @@ declare let window: any;
 })
 export class AppComponent implements OnInit {
   title = 'dapp-frontend';
-  web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+  web3 = new Web3(new Web3.providers.HttpProvider("HTTP://localhost:7545"));
+  web3Api: any = {};
+  provider: any;
   connectedAccount = '';
+  blockchainAccounts: any = [];
 
   ngOnInit(): void {
     this.loadWeb3();
+
   }
 
+  /**
+   * assign web 3 instance in windows object
+   */
   async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
+      this.provider = window.ethereum;
       await window.ethereum.enable;
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
+      this.provider = window.web3.currentProvider;
     } else {
       window.alert('Non-Ethereum browser detected. You Should consider using MetaMask!');
     }
+    this.web3Api = {
+      web3: new Web3(this.provider),
+      provider: this.provider
+    };
   }
 
+  /**
+   * connect to metamask
+   */
   connectToMetamask() {
     this.metamaskConnect();
   }
 
+  /**
+   * get current connected account in metamask
+   */
   async metamaskConnect() {
     this.connectedAccount = await window.ethereum.request({
       method: "eth_requestAccounts"
     });
+  }
+
+  /**
+   * get blockchain accounts
+   */
+  async getBlockchainAccounts() {
+    this.blockchainAccounts = await this.web3.eth.getAccounts();
+    this.connectedAccount = this.blockchainAccounts[0];
+  }
+
+  /**
+   * get metamask accounts
+   */
+  async getMetamaskAccount() {
+    this.blockchainAccounts = await this.web3Api.web3.eth.getAccounts();
+    this.connectedAccount = this.blockchainAccounts[0];
+
   }
 }
